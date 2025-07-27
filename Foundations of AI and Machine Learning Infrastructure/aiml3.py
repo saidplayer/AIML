3c31cb0f4861ea5c6309fdb7f88a6fade68d9254
S <player.said777@gmail.com>
2025-07-27 17:25:53 +0200

    Added all files again

---
+++ Foundations of AI and Machine Learning Infrastructure/aiml3.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
